bfb2d3ad4a52505d75734d14c12fee4909320753
Alex <borovinskihalex986@gmail.com>
2023-06-29 22:12:22 +0500

    sprins file 1.0

---
+++ src/main/java/ru/skypro/lessons/springboot/springf/SpringFApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.springf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFApplication.class, args);
+    }
+
+}
